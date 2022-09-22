4026ae5ffb6c6a0fafa858acc1b82f864016af13
LittleB0xes <nihenriot@gmail.com>
2022-09-22 13:39:20 +0200

    Add camera motion

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use macroquad::rand;
 use macroquad::rand::rand;
 
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::cmp;
 
 const WIDTH: usize = 1280;
 const HEIGHT: usize = 720;
@@ -19,7 +20,10 @@ async fn main() {
     let mut frame_count: u32 = 0;
     let mut board: Vec<bool>;
     let mut temp_board: Vec<bool>;
-    let mut cell_size = 6.0;
+    let mut cell_size = 4.0;
+    let cell_size_limit = (4.0, 20.0);
+
+    let mut camera = Vec2::ZERO;
 
     let width = WIDTH / cell_size as usize;
     let height = (HEIGHT - MARGIN) / cell_size as usize;
@@ -32,7 +36,6 @@ async fn main() {
     temp_board = vec![false; (width * height) as usize];
 
     loop {
-
         if !paused && frame_count % 4 == 0 {
 
             // Update board if not paused
@@ -77,8 +80,8 @@ async fn main() {
         }
         // If paused, draw or remove cells
         else if paused {
-            mouse_cell.x = ((mouse_position().0 - 0.5 * cell_size) / cell_size).round();
-            mouse_cell.y = ((mouse_position().1 - 0.5 * cell_size) / cell_size).round();
+            mouse_cell.x = ((mouse_position().0 - 0.5 * cell_size + camera.x) / cell_size).round();
+            mouse_cell.y = ((mouse_position().1 - 0.5 * cell_size + camera.y) / cell_size).round();
             if is_mouse_button_pressed(MouseButton::Left) {
                 let index = (mouse_cell.x + mouse_cell.y * width as f32) as usize;
                 board[index] = !board[index];
@@ -90,19 +93,18 @@ async fn main() {
         // Draw board
         for (index, cell) in board.iter().enumerate() {
             if *cell {
+                let mut color: Color = WHITE;
+                if paused {color = GRAY}
                 draw_circle_lines(
-                    0.5 * cell_size + (index % width) as f32 * cell_size,
-                    0.5 * cell_size + (index / width) as f32 * cell_size,
+                    0.5 * cell_size - camera.x + (index % width) as f32 * cell_size,
+                    0.5 * cell_size - camera.y + (index / width) as f32 * cell_size,
                     0.4 * cell_size,
                     cell_size / 6.0,
-                    WHITE);
+                    color);
             }
         }
-
-
         // Show some data
         draw_text(&format!("{}", get_fps()), 10.0, 700.0, 16.0, WHITE);
-
 
         // Check inputs
         if is_key_pressed(KeyCode::R) {
@@ -116,10 +118,29 @@ async fn main() {
         }
         else if is_key_pressed(KeyCode::D) {
             cell_size += 1.0;
+            if cell_size > cell_size_limit.1 {cell_size = cell_size_limit.1}
         }
         else if is_key_pressed(KeyCode::S) {
             cell_size -= 1.0;
+            if cell_size < cell_size_limit.0 {cell_size = cell_size_limit.0}
         }
+
+
+        // Camera motion
+        if is_key_down(KeyCode::Up) {
+            camera.y -= cell_size as f32;
+        }
+        else if is_key_down(KeyCode::Down) {
+            camera.y += cell_size as f32;
+        }
+
+        if is_key_down(KeyCode::Left) {
+            camera.x -= cell_size as f32;
+        }
+        else if is_key_down(KeyCode::Right) {
+            camera.x += cell_size as f32;
+        }
+
 
         frame_count += 1; 
         next_frame().await
